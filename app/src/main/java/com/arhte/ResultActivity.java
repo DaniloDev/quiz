4fb7ac8706791c6163f8d7530dc37d137bfbc09a
Danilo Santos Nascimento <danisn2010@gmail.com>
2020-05-19 01:11:27 -0300

    Modificação de kayout pra caber mais texto, aumento dos botões de escolha

--- app/src/main/java/com/arhte/ResultActivity.java
+++ app/src/main/java/com/arhte/ResultActivity.java
@@ -19,6 +19,7 @@ public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_result);
 
         textViewt = findViewById(R.id.textViewtq);
